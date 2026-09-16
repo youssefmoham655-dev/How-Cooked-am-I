32487cec688a0f9292b0b7d1ab56de20e45426dc
youssefmoham655-dev <youssefmoham655@gmail.com>
2026-09-17 02:46:52 +0300

    HTML - changed the place of <script> - putted a value for question CSS - added result section JS - added array of images -result and question areas - function of showing question and arryas of current_index and answers

--- main-cooked-quiz.js
+++ main-cooked-quiz.js
@@ -2,6 +2,8 @@ const full_page = document.getElementById("page") // this for changing backgroun
 const no = document.getElementById("no")
 const yes = document.getElementById("yes")
 const question = document.getElementById("question")
+const result_area = document.getElementById("result")
+const question_area = document.getElementById("question-area")
 
 const questions = [
     "Have you opened your laptop and immediately forgotten why?",
@@ -13,6 +15,45 @@ const questions = [
     "Have you checked your phone while actively holding your phone?",
     "Do you currently have 17 tabs open?",
     "Have you told yourself “tomorrow I lock in” more than once?",
-    "Have you renamed a file instead of actually working on it?",
-    "Have you spent longer choosing music than doing the task?",
+    "Have you renamed a file instead of actually working on it?"
 ]
+
+const images_array = [
+    "assets/1.png",
+    "assets/2.png",
+    "assets/3.png",
+    "assets/4.png",
+    "assets/5.png",
+    "assets/6.png",
+    "assets/7.png",
+    "assets/8.png",
+    "assets/9.png",
+    "assets/10.png",
+    "assets/11.png"
+]
+let current_index = 0
+let answers =[]
+
+
+function showquestion() {
+    if (current_index < questions.length) {
+        question.textContent = questions[current_index]
+        full_page.style.backgroundImage = `url('${images_array[current_index % images_array.length]}')`
+        } else {
+        question_area.style.display = "none"
+        result_area.style.display = "flex"
+        result_area.innerHTML = "<h1>hello</h1>"
+    }
+}
+
+yes.addEventListener('click', () => {
+    answers.push("yes")
+    current_index++
+    showquestion()
+})
+no.addEventListener('click', () => {
+    answers.push("no")
+    current_index++
+    showquestion()
+})
+showquestion()
