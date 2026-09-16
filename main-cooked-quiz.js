4fcea551a81475f58be86d04a7feed662cb54e99
youssefmoham655-dev <youssefmoham655@gmail.com>
2026-09-17 01:24:43 +0300

    adjusted id's names for js and added hover effect for answers and added questions

---
+++ main-cooked-quiz.js
@@ -0,0 +1,18 @@
+const full_page = document.getElementById("page") // this for changing background color
+const no = document.getElementById("no")
+const yes = document.getElementById("yes")
+const question = document.getElementById("question")
+
+const questions = [
+    "Have you opened your laptop and immediately forgotten why?",
+    "Did you say “I’ll do it later” today?",
+    "Have you watched a “5 minute” video that mysteriously lasted 47 minutes?",
+    "Is your Downloads folder a war crime?",
+    "Have you made a productivity system instead of being productive?",
+    "Did you open your assignment, stare at it, then close it?",
+    "Have you checked your phone while actively holding your phone?",
+    "Do you currently have 17 tabs open?",
+    "Have you told yourself “tomorrow I lock in” more than once?",
+    "Have you renamed a file instead of actually working on it?",
+    "Have you spent longer choosing music than doing the task?",
+]
