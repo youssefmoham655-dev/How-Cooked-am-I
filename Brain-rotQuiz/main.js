0cce6a56c47d51d25ea5ffe40ed2d6ccff36033a
hot-stemer <loma.study.ac@gmail.com>
2026-09-20 10:24:35 +0300

    Add initial quiz functionality and question data for Brain-rotQuiz
    - Created main.js with question data structure including questions, images, options, and correct answers.
    - Added notes.md with visual quiz question ideas and a set of 10 quiz questions with varying difficulty levels.
    - Introduced a new file main1.js for potential future enhancements.

---
+++ Brain-rotQuiz/main.js
@@ -0,0 +1,70 @@
+
+questionsData = [
+    {
+        Question : "Who is this character?",
+        image : "image copy.png",
+        options :["Tralalero Tralala", "Bomba Crocodilo", "Jaws Nike", "Blue SPeedster"],
+        currectAnswer : "Tralalero Tralala"
+    }
+
+    {
+        Question: "What sound does the tree monkey make?",
+        image: "image copy 8.png",
+        options: ["Tung Tung", "Brr Brr Patapim", "Tralalero", "Meow"],
+        correctAnswer: "Brr Brr Patapim"
+    }
+    {
+        Question: "",
+        image: "",
+        options:[],
+        correctAnswer:"",
+    }
+        {
+        Question: "",
+        image: "",
+        options:[],
+        correctAnswer:"",
+    }
+        {
+        Question: "",
+        image: "",
+        options:[],
+        correctAnswer:"",
+    }
+        {
+        Question: "",
+        image: "",
+        options:[],
+        correctAnswer:"",
+    }
+        {
+        Question: "",
+        image: "",
+        options:[],
+        correctAnswer:"",
+    }
+        {
+        Question: "",
+        image: "",
+        options:[],
+        correctAnswer:"",
+    }
+]
+//question , images, options , correct answer. 
+// currentQuestion variable 
+//write a loadQuestion function (updatas the html file , grab the current question, change the radio options)
+//function that caculates the resulate ! 
+// the result page that got al the data 
+
+img = document.querySelector("img");
+lael1 = document.getElementById("label1");
+label2 = document.getAnimations("label2");
+label3 = document.getElementById("label3");
+label4 = document.getElementById("label4");
+question = document.querySelector("h1");
+hint = document.getElementById("hint");
+subQuestion = document.querySelector("h3");
+
+let selected = document.querySelector('input[name="answer"]:checked');
+
+
