a1fde68e32e269b5ff4a5450f68afa55dbe37433
youssefmoham655-dev <youssefmoham655@gmail.com>
2026-09-20 21:07:09 +0300

    declared IDs from HTML

---
+++ Is Your Brain On/main-is-ur-brain-on.js
@@ -0,0 +1,6 @@
+const numberOfQuestions = document.getElementById("nofoquestion");
+const questionItSelf = document.getElementById("questionitself");
+const ans1 = document.getElementById("ans1");
+const ans2 = document.getElementById("ans2");
+const ans3 = document.getElementById("ans3");
+const ans4 = document.getElementById("ans4");
